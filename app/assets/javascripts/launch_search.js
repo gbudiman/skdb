7a275d2d837b25cedc0f657735e11a5a99d9d9fc
Gloria Budiman <wahyu.g@gmail.com>
2016-01-18 01:40:33 -0500

    Dynamic search result fully working. Ready for interfacing with compare functionality.

--- app/assets/javascripts/launch_search.js
+++ app/assets/javascripts/launch_search.js
@@ -13,6 +13,7 @@ function launch_search_hero(q) {
     url: '/heros/search/' + q
   }).done(function(res) {
     repopulate_hero_search_result(res);
+    attach_add_to_compare($('#hero-result'), 'hero');
   })
 }
 
@@ -21,6 +22,7 @@ function launch_search_skill(q) {
     url: '/skills/search/' + q
   }).done(function(res) {
     repopulate_skill_search_result(res);
+    attach_add_to_compare($('#skill-result'), 'skill');
   })
 }
 
@@ -32,11 +34,12 @@ function launch_search_atb(q) {
   })
 }
 
-function launch_search_hero_having_atb_effect(e, r) {
+function launch_search_hero_having_atb_effect(e, r, el) {
   $.ajax({
     url: '/heros/fetch/having/atb/effect/' + e + '/target/' + r
   }).done(function(res) {
     repopulate_hhae_search_result(res, e + '_' + r);
+    el.attr('data-has-been-fetched', true);
   })
 }
 
@@ -53,6 +56,7 @@ function repopulate_hero_search_result(d) {
             +   '</span>'
             +   '&nbsp;'
             +   v.rank
+            +   '<a href="#" class="addable-to-compare pull-right" data-hero-id=' + v.id + '><span class="addable-to-compare"></span></a>'
             + '</li>');
   })
 }
@@ -77,6 +81,7 @@ function repopulate_skill_search_result(d) {
             +       '<span class="glyphicon glyphicon-star"></span>'
             +       '&nbsp;'
             +       v.hero_rank
+            +       '<a href="#" class="addable-to-compare pull-right" data-hero-id=' + v.hero_id + '><span class="addable-to-compare"></span></a>'
             +     '</div>'
             +   '</div>'
             + '</li>');
@@ -95,40 +100,129 @@ function repopulate_atb_search_result(d) {
             +      'data-query-effect="' + v.effect + '" '
             +      'data-query-target="' + v.target + '" '
             +      'data-target="#' + composite_attribute + '" href="#">'
-            +     v.effect
-            +     '&nbsp;'
-            +     '<span class="glyphicon glyphicon-screenshot"></span>'
-            +     '&nbsp;'
-            +     v.target
+            +     '<div class="row">'
+            +       '<div class="col-xs-11">'
+            +         v.effect  
+            +       '</div>'
+            +       '<div class="col-xs-1 multi-line-vertical-centered">'
+            +         '<span class="badge pull-right">' + v.count + '</span>'
+            +       '</div>'
+            +     '</div>'
+            +     '<div class="row">'
+            +       '<div class="col-xs-8">'
+            +         '<span class="glyphicon glyphicon-screenshot"></span>'
+            +         '&nbsp;'
+            +         v.target
+            +       '</div>'
+            +       '<div class="col-xs-4">'
+            +         '<span class="pull-right expand-collapse"></span>'
+            +       '</div>'
+            +     '</div>'
             +   '</a>'
-            +   '<span class="badge pull-right">' + v.count + '</span>'
-            +   '<ul class="list-unstyled collapse" id="' + composite_attribute + '">'
+            
+            +   '<ul class="list-unstyled effect-hero-list collapse" id="' + composite_attribute + '">'
             +   '</ul>'
             + '</li>');
   })
 
   attach_atb_effect_search();
+  attach_interactive_expand_button();
 }
 
 function attach_atb_effect_search() {
-  $('a[data-target]').on('click', function() {
-    var effect = $(this).attr('data-query-effect');
-    var target = $(this).attr('data-query-target');
-    launch_search_hero_having_atb_effect(effect, target);
+  $('a[data-target]').on('mouseover', function() {
+    var has_been_fetched = $(this).attr('data-has-been-fetched');
+
+    if (!has_been_fetched) {
+      var effect = $(this).attr('data-query-effect');
+      var target = $(this).attr('data-query-target');
+      launch_search_hero_having_atb_effect(effect, target, $(this));
+    }
   })
 }
 
-function repopulate_hhae_search_result(d, element_target) {
-  console.log(element_target);
-  $('#' + element_target).empty();
-
-  console.log(d);
-  $.each(d, function(i, v) {
-    $('#' + element_target)
-      .append('<li class="list-group-item">'
-            +   '<span class="effect-hero-item">'
-            +   v.hero_name
-            +   '</span>'
-            + '</li>'); 
+function attach_interactive_expand_button() {
+  $('a[data-target]').on('mouseover', function() {
+    attach_expand_collapse_cosmetic($(this));
+  }).on('mouseout', function() {
+    $(this).find('.expand-collapse').text('');
   });
+
+  $('ul.effect-hero-list').on('hide.bs.collapse show.bs.collapse', function() {
+    attach_expand_collapse_cosmetic($(this).parent().find('a[data-target]'));
+  });
+}
+
+function attach_expand_collapse_cosmetic(el) {
+  if (el.attr('aria-expanded') == "true") {
+    el.find('.expand-collapse')
+      .html('Collapse <span class="glyphicon glyphicon-chevron-up"></span>');
+  } else {
+    el.find('.expand-collapse')
+      .html('Expand <span class="glyphicon glyphicon-chevron-down"></span>');
+  }
+}
+
+function repopulate_hhae_search_result(d, element_target) {
+  $('#' + element_target).empty();
+  var effect_filter = $('#' + element_target).parent().find('a[data-query-effect]').attr('data-query-effect');
+  var target_filter = $('#' + element_target).parent().find('a[data-query-target]').attr('data-query-target');
+
+  var s = '';
+  $.each(d, function(i, v) {
+    s += '<li class="list-group-item">'
+       +   '<span class="effect-hero-item">' + v.hero_name + '</span>&nbsp;'
+       +   '<span class="glyphicon glyphicon-star"></span>&nbsp;' + v.hero_rank
+       +   '<a href="#" class="addable-to-compare pull-right" data-hero-id=' + v.hero_id + '>'
+       +     '<span class="addable-to-compare"></span>'
+       +   '</a>';
+    
+    $.each(v.skills, function(k_skills, v_skills) {
+      $.each(v_skills['attributes'], function(k_atbs, v_atbs) {
+        if (effect_filter == v_atbs.effect && target_filter == v_atbs.target) {
+          s += '<br />';
+          s += '<span>' + v_skills.name + '</span>';
+          s += '<span class="pull-right">' + v_skills.category;
+
+          if (v_skills.category != 'passive') {
+            s += '&nbsp;<span class="glyphicon glyphicon-hourglass"></span>&nbsp;';
+            s += v_skills.cooldown + 's';
+          }
+
+          s += '</span>';
+        }
+
+
+      })
+    });
+    s += '</li>';
+  });
+
+  $('#' + element_target).append(s);
+  attach_add_to_compare($('#' + element_target), 'effect');
+}
+
+function attach_add_to_compare(el, type) {
+  el.find('span.addable-to-compare').each(function() {
+    var placeholder = $(this);
+
+    if (type == 'hero') {
+      var target = $(this).parent().parent();
+    } else if (type == 'skill') {
+      var target = $(this).parent().parent().parent().parent();
+    } else if (type == 'effect') {
+      var target = $(this).parent().parent();
+    }
+
+    target.on('mouseover', function() {
+      placeholder
+        .html('<span class="glyphicon glyphicon-plus"></span>&nbsp;Compare');
+    }).on('mouseout', function() {
+      placeholder.text('');
+    });
+  });
+
+  $('a.addable-to-compare').off('click').on('click', function() {
+    console.log($(this).attr('data-hero-id'));
+  })
 }
