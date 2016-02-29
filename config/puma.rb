847ce3fe08f5e5963ecd1e7f28598b9c01e127aa
Gloria Budiman <kz208@cummins.com>
2016-02-29 09:54:54 -0500

    Fix conflict in puma.rb config.

--- config/puma.rb
+++ config/puma.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 if Rails.env.production?
   workers Integer(ENV['WEB_CONCURRENCY'] || 2)
   threads_count = Integer(ENV['MAX_THREADS'] || 5)
@@ -15,20 +14,4 @@ if Rails.env.production?
     # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
     ActiveRecord::Base.establish_connection
   end
-=======
-workers Integer(ENV['WEB_CONCURRENCY'] || 2)
-threads_count = Integer(ENV['MAX_THREADS'] || 4)
-threads threads_count, threads_count
-
-preload_app!
-
-rackup      DefaultRackup
-port        ENV['PORT']     || 3000
-environment ENV['RACK_ENV'] || 'development'
-
-on_worker_boot do
-  # Worker specific setup for Rails 4.1+
-  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
-  ActiveRecord::Base.establish_connection
->>>>>>> 511cbc9d4941dfdeae8f969132a30a842663a4e9
 end
