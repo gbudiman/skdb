e762858f3f8ccda73557316a3d345030736f94cc
Gloria Budiman <kz208@cummins.com>
2016-03-02 16:57:06 -0500

    Expanded cumulative stat.

--- app/controllers/visitor_controller.rb
+++ app/controllers/visitor_controller.rb
@@ -4,6 +4,8 @@ class VisitorController < ApplicationController
     s[:unique_visit_count] = Visitor.group(:todays_date).count(:address)
     s[:total_visit_count] = Visitor.group(:todays_date).sum(:todays_count)
     s[:most_active] = Hash[Visitor.group(:address).sum(:todays_count).sort_by { |k, v| -v }.first(32)]
+    s[:unique_visit_to_date] = Visitor.distinct(:address).count(:address)
+    s[:total_visit_to_date] = Visitor.sum(:todays_count)
 
     render json: s
   end
