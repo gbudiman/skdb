deefa47ee00d0f6b9e0b1b9b8cd908364a646d17
Gloria Budiman <kz208@cummins.com>
2016-03-07 13:33:16 -0500

    Coalesce null country names.

--- app/models/ip_country.rb
+++ app/models/ip_country.rb
@@ -10,7 +10,7 @@ class IpCountry < ActiveRecord::Base
     end
 
     IpCountry.joins(:country)
-             .select('countries.long AS country_full_name')
+             .select('COALESCE(countries.long, "--") AS country_full_name')
              .find_by('address_start < :x AND address_end > :x', x: x)
 
   end
