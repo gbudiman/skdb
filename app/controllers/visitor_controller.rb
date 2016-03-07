fd350225fc8743043247bda074c39abbeafb5a58
Gloria Budiman <kz208@cummins.com>
2016-03-07 14:11:10 -0500

    Incorrect parameter passing in VisitorController

--- app/controllers/visitor_controller.rb
+++ app/controllers/visitor_controller.rb
@@ -1,12 +1,12 @@
 class VisitorController < ApplicationController
-  def stats_fetch _limit = 32
+  def stats_fetch
     s = Hash.new
     s[:unique_visit_count] = Visitor.group(:todays_date).order(todays_date: :desc).limit(16).count(:address)
     s[:total_visit_count] = Visitor.group(:todays_date).order(todays_date: :desc).limit(16).sum(:todays_count)
     # s[:most_active] = Hash[Visitor.group(:address).sum(:todays_count).sort_by { |k, v| -v }.first(16)]
     s[:unique_visit_to_date] = Visitor.distinct(:address).count(:address)
     s[:total_visit_to_date] = Visitor.sum(:todays_count)
-    s[:total_visit_by_country] = Visitor.total_visit_by_country _limit
+    s[:total_visit_by_country] = Visitor.total_visit_by_country params[:limit]
 
     render json: s
   end
