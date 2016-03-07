dec3f5eacbf45781a7e3301c21b1982dbb21ae36
Gloria Budiman <kz208@cummins.com>
2016-03-07 15:52:22 -0500

    Disable Geolocation IP for now. Too much overhead.

--- app/models/visitor.rb
+++ app/models/visitor.rb
@@ -26,39 +26,34 @@ class Visitor < ActiveRecord::Base
     out = Hash.new
     result = Hash.new
 
-    # Hash[Visitor.group(:address).sum(:todays_count).sort_by { |k, v| -v }.first(_limit)].each do |address, sum|
-    #   country = IpCountry.seek_address(address)
-    #   country_full_name = country ? country.country_full_name : '<< Unknown >>'
-    #   h[address] = { country: country_full_name, sum: sum }
+    # ip_countries = Hash.new
+    # IpCountry.all.each do |r|
+    #   ip_countries[r.address_start] = {
+    #     address_end: r.address_end,
+    #     country_id: r.country_id,
+    #   }
     # end
 
-    ip_countries = Hash.new
-    IpCountry.all.each do |r|
-      ip_countries[r.address_start] = {
-        address_end: r.address_end,
-        country_id: r.country_id,
-      }
-    end
 
-    addresses = Visitor.group(:address).sum(:todays_count)
-    addresses.each do |_address, _sum|
-      address = IpCountry.parse_integer _address
+    # addresses = Visitor.group(:address).sum(:todays_count)
+    # addresses.each do |_address, _sum|
+    #   address = IpCountry.parse_integer _address
 
-      ip_countries.select { |k, v| k < address and v[:address_end] > address }.each do |k, v|
-        result[v[:country_id]] ||= 0
-        result[v[:country_id]]  += _sum
-      end
-    end
+    #   ip_countries.select { |k, v| k < address and v[:address_end] > address }.each do |k, v|
+    #     result[v[:country_id]] ||= 0
+    #     result[v[:country_id]]  += _sum
+    #   end
+    # end
 
-    countries_lut = Hash.new
+    # countries_lut = Hash.new
 
-    Country.where(id: result.keys).each do |r|
-      countries_lut[r.id] = r.long
-    end
+    # Country.where(id: result.keys).each do |r|
+    #   countries_lut[r.id] = r.long
+    # end
 
-    result.keys.each do |k|
-      out[countries_lut[k]] = result[k]
-    end
+    # result.keys.each do |k|
+    #   out[countries_lut[k]] = result[k]
+    # end
 
     return Hash[out.sort_by { |k, v| -v }]
   end
