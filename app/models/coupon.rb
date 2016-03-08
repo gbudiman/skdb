cd51281b0d77ecd63bd6fc7838c7b6481bf01a60
Gloria Budiman <wahyu.g@gmail.com>
2016-03-07 23:42:57 -0500

    Clarify Coupon.rebuild_database!

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,25 +1,27 @@
 class Coupon < ActiveRecord::Base
   def self.rebuild_database!
-    ActiveRecord::Base.transaction do
-      Coupon.delete_all
+    Utility.shut_up do
+      ActiveRecord::Base.transaction do
+        Coupon.delete_all
 
-      File.open(Rails.root.join('db', 'coupons.csv')) do |f|
-        f.each_line do |l|
-          next if l[0] == '#'
+        File.open(Rails.root.join('db', 'coupons.csv')) do |f|
+          f.each_line do |l|
+            next if l[0] == '#'
 
-          cells = l.gsub(/\n/, '').split(/\,/)
-          input_code, reward, is_expired, instruction, credits = cells
+            cells = l.gsub(/\n/, '').split(/\,/)
+            input_code, reward, is_expired, instruction, credits = cells
 
 
-          Coupon.create! input_code: input_code,
-                         reward: reward,
-                         is_expired: is_expired,
-                         instruction: instruction,
-                         credits: credits
+            Coupon.create! input_code: input_code,
+                           reward: reward,
+                           is_expired: is_expired,
+                           instruction: instruction,
+                           credits: credits
+          end
         end
       end
     end
 
-    return Coupon.all
+    puts "#{Coupon.count} coupons exist"
   end
 end
